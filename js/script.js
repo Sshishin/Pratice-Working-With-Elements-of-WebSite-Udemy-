3d2a672355a964d731af289ecb31879745a7034f
Sergey Shishin <flyers32rus@gmail.com>
2022-08-05 17:21:16 +0300

    example #48

--- js/script.js
+++ js/script.js
@@ -1,50 +1,118 @@
-/* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+document.addEventListener('DOMContentLoaded', () => {  //КОД СРАБОТАЕТ ТОЛЬКО ТОГДА КОГДА ДОМ СТРУКТУРА УЖЕ БУДЕТ ЗАГРУЖЕНА ЧТОБЫ ВСЕ ПРОГРУЗИТЬСЯ УСПЕЛО
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+    'use strict';
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    
+    const rem = document.querySelectorAll('.promo__adv img'),     
+        gen = document.querySelector('.promo__genre'),
+        poster = document.querySelector('.promo__bg'),
+        list = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = document.querySelector('.adding__input'),
+        checkbox = document.querySelector('[type="checkbox"]');
+    
 
-5) Добавить нумерацию выведенных фильмов */
+    //Обернули в функцию
+    const deleteAdv = (arr) => {
+        arr.forEach( (item) => {
+            item.remove();
+            });
+    };
+    
+  
 
-'use strict';
+        
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+    //Обернули в функцию
+    const makeChanges = () => {
+        gen.textContent = 'ДРАМА';
+    
+        poster.style.backgroundImage = 'url("img/bg.jpg")' ;  
+    };
 
-// Удалили элементы со страницы
+    
 
-const rem = document.querySelectorAll('.promo__adv img'),       //Так обращаемся к дочернему элементу
-    gen = document.querySelector('.promo__genre'),
-    poster = document.querySelector('.promo__bg'),
-    list = document.querySelector('.promo__interactive-list');
 
-rem.forEach( (item) => {
-item.remove();
+     //Обернули в функцию
+    const sortArr = (arr) => {
+        arr.sort(); 
+    };
+
+    
+    
+
+    //Обернули в функцию
+    function createMovieList(films, parent) {
+        parent.innerHTML = '';        
+        sortArr(films);
+        
+        films.forEach((num,item) => {      
+            parent.innerHTML += `<li class="promo__interactive-item">${item + 1}. ${num}
+            <div class="delete"></div
+            ></li>`;    
+        });
+        
+        // Удаляем элементы при клике на корзину
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);  //КАК ТУТ РАБОТАЕТ РЕКУРСИЯ?
+            });
+        });
+    }
+
+    
+
+    //Свежее решение 
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();         //Делаем чтобы страница не перезагружалась при вводе в инпут
+   
+
+    let newFilm = addInput.value;
+    const favorite = checkbox.checked;
+
+    if(newFilm) {
+
+        if(newFilm.length > 21) {
+            newFilm = `${newFilm.substring(0,22)}...`;
+        }
+
+        if(favorite) {
+            console.log('Вы добавили любимый фильм')
+        }
+        
+        movieDB.movies.push(newFilm);       //Добавляем переменную с новым фильмом в массив
+    sortArr(movieDB.movies);        //Используем функцию сортировки повторно
+
+    createMovieList(movieDB.movies, list);      //Здесь таким образом добавляем новый фильм?
+    }
+
+    
+
+
+
+    event.target.reset();
+    });
+
+    deleteAdv(rem);
+    makeChanges();
+    
+    createMovieList(movieDB.movies, list);
 });
 
-gen.textContent = 'ДРАМА';
-
-poster.style.backgroundImage = 'url("img/bg.jpg")' ;        //Разные кавычки это тут важно
-
-list.innerHTML = '';        //Так как пустые ковычки то таким образом мы тоже можем удалять кусочек кода
-
-movieDB.movies.sort();      //Сортируем по алфавиту
-
-movieDB.movies.forEach((num,item) => {      //Начинаем перебирать именно массив потому что его значения будем использовать
-    list.innerHTML += `<li class="promo__interactive-item">${item + 1}. ${num}
-    <div class="delete"></div
-    ></li>`;    //Обращаемся к месту куда нужно добавить необходимый код и ставим += чтобы код добавлялся новыми строками
-});
